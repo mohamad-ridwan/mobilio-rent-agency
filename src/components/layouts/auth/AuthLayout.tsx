5228f0e39827274c5ccf07ec0274112cfd3716d5
mohamad-ridwan <mr643062@gmail.com>
2025-05-13 07:37:25 +0700

    style: update AuthLayout and globals.css for improved layout and overflow handling

--- src/components/layouts/auth/AuthLayout.tsx
+++ src/components/layouts/auth/AuthLayout.tsx
@@ -1,33 +1,39 @@
 // components/layouts/AuthLayout.tsx
 
-import { ReactNode } from 'react';
-import Image from 'next/image';
+import { ReactNode } from "react";
+import Image from "next/image";
 import { cn } from "@/lib/utils"; // dari shadcn, buat merge class
 
 interface AuthLayoutProps {
   children: ReactNode;
-  prefixImg: string
+  prefixImg: string;
   imageSrc?: string;
   imageAlt?: string;
 }
 
-export default function AuthLayout({ children, prefixImg, imageSrc, imageAlt = "Auth Mobilio Rent" }: AuthLayoutProps) {
-    
-    const imageSrcCurrently = `${prefixImg}/${imageSrc}`
+export default function AuthLayout({
+  children,
+  prefixImg,
+  imageSrc,
+  imageAlt = "Auth Mobilio Rent",
+}: AuthLayoutProps) {
+  const imageSrcCurrently = `${prefixImg}/${imageSrc}`;
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-dvh">
       {/* LEFT - Form */}
-      <div className="w-full lg:w-1/2 flex items-center justify-center p-8 bg-neutral-50 dark:bg-neutral-950">
-        <div className="max-w-md w-full space-y-6">
+      <div className="w-full lg:w-1/2 flex items-center justify-center p-8 bg-neutral-50 dark:bg-neutral-950 overflow-y-auto max-h-dvh">
+        <div className="max-w-md w-full space-y-6 max-h-full py-4">
           {children}
         </div>
       </div>
 
       {/* RIGHT - Image */}
-      <div className={cn(
-        "hidden lg:block relative w-1/2",
-        "bg-neutral-100 dark:bg-neutral-900"
-      )}>
+      <div
+        className={cn(
+          "hidden lg:block relative w-1/2",
+          "bg-neutral-100 dark:bg-neutral-900"
+        )}
+      >
         <Image
           src={imageSrcCurrently}
           alt={imageAlt}

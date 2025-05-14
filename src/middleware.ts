93d55e6bebe562674086f89eb0b045b24ff5c78a
mohamad-ridwan <mr643062@gmail.com>
2025-05-14 07:33:18 +0700

    feat: add middleware for session management and route protection

---
+++ src/middleware.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+import { mobilioSession } from "./constants/storage";
+import { cookies } from "next/headers";
+
+export const config = {
+  matcher: [
+    /*
+     * Apply middleware to all routes except:
+     * - API routes
+     * - _next (static assets)
+     * - static files like images from /public
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|images|icons).*)",
+  ],
+};
+
+export async function middleware(request: NextRequest) {
+  const cookieStore = await cookies();
+  const auth_session = cookieStore.get(mobilioSession);
+
+  const nextResponse = NextResponse.next();
+
+  if (
+    request.nextUrl.pathname.startsWith("/assets") ||
+    request.nextUrl.pathname.startsWith("/public")
+  ) {
+    return nextResponse;
+  }
+
+  if (
+    (request.url.includes("/auth/login") ||
+      request.url.includes("/auth/register")) &&
+    auth_session?.value
+  ) {
+    const response = NextResponse.redirect(
+      new URL(`/dashboard/statistics`, request.url)
+    );
+    return response;
+  }
+
+  if (
+    !auth_session?.value &&
+    (!request.url.includes("/auth/login") ||
+      !request.url.includes("/auth/register"))
+  ) {
+    if (
+      request.url.includes("/auth/login") ||
+      request.url.includes("/auth/register")
+    ) {
+      return nextResponse;
+    }
+    const response = NextResponse.redirect(new URL(`/auth/login`, request.url));
+    return response;
+  }
+  return nextResponse;
+}

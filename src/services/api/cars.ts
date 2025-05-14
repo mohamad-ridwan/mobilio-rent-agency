ceaf214ccebb2e21cf2a68fb1b1c33884aef14c6
mohamad-ridwan <mr643062@gmail.com>
2025-05-14 10:34:50 +0700

    feat: enhance fetchCars function to include authorization header using session cookies

--- src/services/api/cars.ts
+++ src/services/api/cars.ts
@@ -3,14 +3,19 @@
 import { fetchApi } from "@/services/api/fetchApi";
 import { APIBaseURL } from "./baseUrl";
 import { CarListResponseType } from "@/types/api/cars";
+import { cookies } from "next/headers";
+import { mobilioSession } from "@/constants/storage";
 
 export async function fetchCars(): Promise<CarListResponseType> {
+  const cookieStore = await cookies();
+  const auth_session = cookieStore.get(mobilioSession);
   const res = await fetchApi<CarListResponseType>(
     `${APIBaseURL}/agencies/cars/lists`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${auth_session?.value}`,
       },
     }
   );
